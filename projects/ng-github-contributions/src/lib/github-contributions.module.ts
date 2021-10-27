bd08a9ac1491c3b24d63bd5115760a15c8d58bc1
Luiz Philippe <luizphilippep@gmail.com>
2021-10-27 18:37:15 -0300

    Remove function call from module import

--- projects/ng-github-contributions/src/lib/github-contributions.module.ts
+++ projects/ng-github-contributions/src/lib/github-contributions.module.ts
@@ -6,9 +6,6 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BoardColumnComponent } from './components/github-board/board-column/board-column.component';
 import { BoardLegendComponent } from './components/github-board/board-legend/board-legend.component';
 import { TooltipModule } from 'ng2-tooltip-directive';
-import { DefaultTooltipOptions } from './constants';
-
-const TooltipImport = TooltipModule.forRoot(DefaultTooltipOptions);
 
 @NgModule({
   declarations: [
@@ -20,7 +17,7 @@ const TooltipImport = TooltipModule.forRoot(DefaultTooltipOptions);
   imports: [
     CommonModule,
     NgbModule,
-    TooltipImport,
+    TooltipModule,
   ],
   exports: [
     GithubBoardComponent,
